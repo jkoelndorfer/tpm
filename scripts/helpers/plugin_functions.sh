287f1f5f049a5b6c6a17989e19cbe6dcb5466a5b
John Koelndorfer <john@johnk.io>
2021-10-10 04:12:23 -0500

    Use envsubst to expand sourced file paths
    This allows for the use of environment variables other than $HOME in
    sourced file paths.

--- scripts/helpers/plugin_functions.sh
+++ scripts/helpers/plugin_functions.sh
@@ -5,7 +5,7 @@ tpm_plugins_variable_name="@tpm_plugins"
 _manual_expansion() {
 	local path="$1"
 	local expanded_tilde="${path/#\~/$HOME}"
-	echo "${expanded_tilde/#\$HOME/$HOME}"
+	envsubst <<< "$path"
 }
 
 _tpm_path() {
